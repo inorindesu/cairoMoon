cb6c5e08dbcf71cd1939c85191c5b9a4b69dfadc
Inori Sakura <inorindesu@gmail.com>
2013-04-28 21:37:02 +0800

    Added lua codes.
    The lua codes can be loaded and run by the main program.

---
+++ square-fillOnly.lua
@@ -0,0 +1,11 @@
+outputPath = "/tmp/square-fillOnly.pdf"
+stroke = false
+fill = true
+
+function drawPath()
+  moveTo(30, 30)
+  lineTo(40, 30)
+  lineTo(40, 40)
+  lineTo(30, 40)
+  close()
+end
---
+++ square-strokeNfill.lua
@@ -0,0 +1,11 @@
+outputPath = "/tmp/square-strokeNfill.pdf"
+stroke = true
+fill = true
+
+function drawPath()
+  moveTo(30, 30)
+  lineTo(40, 30)
+  lineTo(40, 40)
+  lineTo(30, 40)
+  close()
+end
